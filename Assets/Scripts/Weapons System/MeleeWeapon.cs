a14aee5272e090839b06765ee585846b26618111
ahmedtarek52 <a7mdtarek52@gmail.com>
2025-05-20 14:44:41 +0300

    setup mechanics of melee attack

--- Assets/Scripts/Weapons System/MeleeWeapon.cs
+++ Assets/Scripts/Weapons System/MeleeWeapon.cs
@@ -1,19 +1,33 @@
+using System.Collections;
 using UnityEngine;
 
 public class MeleeWeapon : Weapon
 {
   public int damage;
   public float attackRange;
+  public Collider hitbox;
+  public float attackDuration = 0.3f;
 
     private void Awake()
     {
         weaponType = WeaponType.Melee;
+        if (hitbox != null) hitbox.enabled = false;
     }
     public override void Use()
     {
+        StartCoroutine(PerformAttack());
 
     }
+    IEnumerator PerformAttack()
+    {
+        Debug.Log("Swinging melee weapon...");
+        hitbox.enabled = true;
 
+        yield return new WaitForSeconds(attackDuration);
+
+        hitbox.enabled = false;
+        Debug.Log("Attack finished.");
+    }
     public override void Reload()
     {
         // Implement reloading logic here
