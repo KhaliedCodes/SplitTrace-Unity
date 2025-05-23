9aebb6dc1df830bc679d3c100adfef43bd7dbb09
MAhmedSA <mohamed.asa.ahmed@gmail.com>
2025-05-23 21:13:15 +0300

    AA
    aaa

--- Assets/Scripts/LogsSystem/LogsSystem.cs
+++ Assets/Scripts/LogsSystem/LogsSystem.cs
@@ -2,13 +2,14 @@ using UnityEngine;
 
 public class LogsSystem : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    int numberOfLogs ;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
