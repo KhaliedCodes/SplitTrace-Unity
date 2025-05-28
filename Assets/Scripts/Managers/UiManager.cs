0e4ac923bf0812e2eecb3d452737121f02204fdf
Khalied <khaliedmails@gmail.com>
2025-05-28 22:45:40 +0300

    Added ui manager

---
+++ Assets/Scripts/Managers/UiManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class UiManager : MonoBehaviour
+{
+    public static UiManager Instance;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
