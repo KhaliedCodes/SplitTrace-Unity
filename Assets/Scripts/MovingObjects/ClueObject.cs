d275509d3184e99b74cf702de05df881443479a1
Ahmad1704 <101527171+Ahmad1704@users.noreply.github.com>
2025-06-15 12:11:37 +0300

    item description

--- Assets/Scripts/MovingObjects/ClueObject.cs
+++ Assets/Scripts/MovingObjects/ClueObject.cs
@@ -2,32 +2,90 @@ using UnityEngine;
 
 public class ClueObject : MonoBehaviour
 {
-    public bool Highlighted = false; // Indicates if the clue object is highlighted
-    [SerializeField] private Material highlightMaterial; // Material to use when highlighted
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    [Header("Highlighting")]
+    public bool Highlighted = false;
+    [SerializeField] private Material highlightMaterial;
+    private Material originalMaterial;
 
+    [Header("Clue Data")]
+    public ClueData clueData;
+
+    [Header("UI References")]
+    [SerializeField] private ItemDescriptionUI itemUI;  // Shows full description
+    [SerializeField] private GameObject pressEPromptUI; // "Press E" UI
+
+    private bool playerInTrigger = false;
+    private bool descriptionVisible = false;
+
+    private void Start()
+    {
+        originalMaterial = GetComponent<Renderer>().material;
+
+        if (itemUI == null)
+            itemUI = FindFirstObjectByType<ItemDescriptionUI>();
+
+        if (pressEPromptUI != null)
+            pressEPromptUI.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-
+        if (playerInTrigger && Highlighted && Input.GetKeyDown(KeyCode.E))
+        {
+            ShowDescription();
+        }
     }
-    
+
+    private void ShowDescription()
+    {
+        descriptionVisible = true;
+        pressEPromptUI?.SetActive(false);
+        itemUI?.Show(clueData);
+    }
+
+    private void HideDescription()
+    {
+        descriptionVisible = false;
+        itemUI?.Hide();
+    }
+
     public void SetHighlighted(bool highlighted)
     {
         Highlighted = highlighted;
+
         if (highlighted)
         {
-            // Apply the highlight material
             GetComponent<Renderer>().material = highlightMaterial;
+
+            if (playerInTrigger && !descriptionVisible)
+                pressEPromptUI?.SetActive(true);
         }
         else
         {
-            // Reset to the original material (assuming it is stored or can be retrieved)
-            GetComponent<Renderer>().material = GetComponent<Renderer>().sharedMaterial;
+            GetComponent<Renderer>().material = originalMaterial;
+            HideDescription();
+            pressEPromptUI?.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInTrigger = true;
+
+            if (Highlighted && !descriptionVisible)
+                pressEPromptUI?.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInTrigger = false;
+            pressEPromptUI?.SetActive(false);
+            HideDescription();
         }
     }
 }
